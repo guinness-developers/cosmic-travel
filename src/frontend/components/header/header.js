5b21271810d5caa190964bfcf2ce6fdb252d752c
Daniel Bevziuc <bevziuc.daniel@gmail.com>
2019-03-23 22:10:10 +0000

    added nav bar

---
+++ src/frontend/components/header/header.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className='header'>
+          <div className='container'>
+            <div className='nav-bar'>
+                <div className='logo'>
+                    cosmic
+                </div>
+                <nav className='nav'>
+                    <a href='https://www.google.com/' 
+                        target="_blank"
+                        rel='noopener noreferrer'>Log In</a>
+                </nav>
+            </div>
+          </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
