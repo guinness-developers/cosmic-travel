ef1ba67ea780b30356447a9ca318b9173a8aab97
alexeiscript <suzakukururug1@yandex.com>
2019-03-23 22:39:33 +0000

    added banner component

---
+++ src/frontend/components/main/banner/banner.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './banner.css';
+
+class Banner extends Component {
+  render() {
+    return (
+      <section className="banner">
+        <div className="banner-content">
+          <div className="content-left">
+            <h1 className="intro">Explore the solar system with us</h1>
+            <h4 className="intro">Explore the solar system with us</h4>
+            <div className="buttons">
+
+            </div>
+          </div>
+          <div className="content-right">
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Banner;
