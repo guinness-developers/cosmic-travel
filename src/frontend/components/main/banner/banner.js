b88adc594e40cc7dae458a842ac4fb71894cb09f
alexeiscript <suzakukururug1@yandex.com>
2019-03-24 10:42:51 +0000

    added buttons, background image

--- src/frontend/components/main/banner/banner.js
+++ src/frontend/components/main/banner/banner.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import './banner.css';
+import TripsButton from '../buttons/tripsBtn';
+import RegisterButton from '../buttons/registerBtn';
+
 
 class Banner extends Component {
   render() {
     return (
       <section className="banner">
-        <div className="banner-content">
-          <div className="content-left">
-            <h1 className="intro">Explore the solar system with us</h1>
-            <h4 className="intro">Explore the solar system with us</h4>
-            <div className="buttons">
-
-            </div>
-          </div>
-          <div className="content-right">
+        <div className="container">
+          <div className="banner-content">
+              <h1 className="intro">Explore the solar<br /> system with us</h1>
+              <h4 className="intro">We take you to the planets in our <br />solar system, offering you once in a <br />lifetime experiences that you will <br />never forget.</h4>
+              <div className="buttons">
+              <TripsButton />
+              <RegisterButton />
+              </div>
           </div>
         </div>
       </section>
