0aadec7a117992744e5d070b8313ad0581da0efe
alexeiscript <suzakukururug1@yandex.com>
2019-03-25 22:27:54 +0000

    created card component

---
+++ src/data/cardsData/cardsData.js
@@ -0,0 +1,32 @@
+import marsImage from './productsImages/mars.jpg';
+import cartonImg from './dataImg/carton.png';
+import buildImg from './dataImg/part.png';
+import teslaImg from './dataImg/tesla.png';
+
+let Portfolio = [
+  {
+    tripTitle: 'Trip to Mars',
+    image: marsImage,
+    description: 'I build that APP on Kodiri boot camp, that app is was team app.'
+  },
+  {
+    name: 'carton plant',
+    link: 'https://daniel-bevziuc.github.io/carton-plant/',
+    img: cartonImg,
+    description: 'I build that app for cardboard plant',
+  },
+  {
+    name: 'build part',
+    link: 'https://daniel-bevziuc.github.io/construction-part.github.io/',
+    img: buildImg,
+    description: 'I build that app for a build part company',
+  },
+  {
+    name: 'tesla',
+    linik: 'https://daniel-bevziuc.github.io/auto-agent-app/',
+    img: teslaImg,
+    descriptio: 'I build that app for auto agent'
+  },
+];
+
+export default Portfolio
--- src/frontend/components/main/products/products.js
+++ src/frontend/components/main/products/products.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './products.sass';
+import MediaCard from './card/card';
+import tripImage from './card/productImages/mars.jpg';
 
 
 export default function Products(props) {
     return (
       <section className="products">
         <div className="">
-          <div className="">
-
-          </div>
+          <MediaCard
+            images={tripImage}
+            tripTitle="Trip to Mars"
+            description="Enjoy a 6 months round trip to the red planet"
+          />
         </div>
       </section>
     );
