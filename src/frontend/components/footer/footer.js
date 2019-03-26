a2724d4ea2e778810511047a5058702712cdfd9a
Alexei Stavila <alexei.stavila@design-portfolio.co.uk>
2019-03-26 10:48:36 +0000

    created footer component

---
+++ src/frontend/components/footer/footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './footer.sass';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+        
+      </div>
+    );
+  }
+}
+
+export default Footer;
