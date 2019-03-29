e21186f7a23e8ec87ec762b7cdde16eb74bf5f7e
Alexei Stavila <alexei.stavila@design-portfolio.co.uk>
2019-03-29 17:26:30 +0000

    layout and styling

--- src/frontend/components/main/main.js
+++ src/frontend/components/main/main.js
@@ -6,12 +6,14 @@ import Products from './products/products';
 class Main extends Component {
   render() {
     return (
-      <Fragment>
-        <Banner
-          introTitle="Explore the solar system with us"
-          introParagraph="We take you to the planets in our solar system, offering you once in a lifetime experiences that you will never forget."/>
-        <Products />
-      </Fragment>
+      <div className="wrapper">
+        <Fragment>
+          <Banner
+            introTitle="Explore the solar system with us"
+            introParagraph="We take you to the planets in our solar system, offering you once in a lifetime experiences that you will never forget."/>
+          <Products />
+        </Fragment>
+      </div>
     );
   }
 }
